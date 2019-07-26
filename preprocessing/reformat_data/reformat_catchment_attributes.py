232377c3393fa2212579e606bc7d5833f5db228b
Jehn <gh2668@uni-giessen.de>
2019-07-26 14:41:02 +0200

    Wrote method to classify the numerical attributes

--- preprocessing/reformat_data/reformat_catchment_attributes.py
+++ preprocessing/reformat_data/reformat_catchment_attributes.py
@@ -6,19 +6,12 @@ Created on Fri Jul 26 13:13:19 2019
 """
 
 import pandas as pd
-from functools import reduce
 import os
 import numpy as np
 # Get the location of this file
 file_dir = os.path.dirname(os.path.abspath(__file__))
 
 def read_attributes():
-    # Go to the directoy of the data
-    # Three dirs up
-    three_up = os.path.abspath(os.path.join(file_dir, os.pardir+os.sep+os.pardir+os.sep+os.pardir))
-    # now locate raw data
-    os.chdir(three_up+os.sep+"Rohdaten")
-    # read
     att_df = pd.read_csv("all_catchment_attributes.csv", encoding='latin-1', sep=";")
     return att_df
 
@@ -35,5 +28,25 @@ def classify_numerical(df, bins, skip_cols, labels):
         df[col+"_cat"] = pd.cut(df[col], bins=bins, labels=labels)
     return df
 
+
+def classify_narrowness(att_df):
+    """
+    Classifies the narrowness of the catchments
+    """
+    att_df["ratio"] = att_df["laenge"] / att_df["breite"]
+  #  att_df["narrowness"] = "equal_sided"
+    # from https://stackoverflow.com/questions/44659040/pandas-assign-category-based-on-where-value-falls-in-range    
+    def cat(x):
+        if x > 1.2 and x < 1.4 or x < 0.8 and x > 0.6:
+            return "narrow"
+        if x > 1.4 or x < 0.6:
+            return "very narrow"
+        return "equal sided"
+
+    att_df["narrowness"] = att_df["ratio"].apply(lambda x: cat(x))
+    return att_df
+    
+
 att_df = read_attributes()
-df = classify_numerical(att_df, 3, ["gauge_id", "breite", "laenge"], ["lowest_third", "middle_third", "highest_third"])
+att_df = classify_narrowness(att_df)
+att_df_classified = classify_numerical(att_df, 3, ["gauge_id", "breite", "laenge", "ratio"], ["lowest_third", "middle_third", "highest_third"])
