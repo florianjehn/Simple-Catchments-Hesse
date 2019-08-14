813a59e08fed56e1cbb6fe19eed70cdbbb711710
Jehn <gh2668@uni-giessen.de>
2019-08-14 14:41:39 +0200

    Made it faster by removing for loops

--- zuecco_hysteresis/hysteresis_scipt.py
+++ zuecco_hysteresis/hysteresis_scipt.py
@@ -7,12 +7,19 @@ Script for the computation of the hysteresis index by G. Zuecco (2016)
 """
 import pandas as pd
 import numpy as np
+import time
 
 
 def hysteresis_class(x:pd.Series,y:pd.Series,x_fixed:pd.Series):
     """
     Finds hysteresis class for x and y
     """
+    if x.isna().any() or y.isna().any():
+        print("x or y contain nan, cannot determine hysteresis class, returning nan")
+        diff_area, h, hyst_class = [pd.Series(np.nan, index=x_fixed.index), 
+                                              np.nan, np.nan]
+        return diff_area, h, hyst_class
+        
     x_norm = normalize(x)
     y_norm = normalize(y)
     x_rise_h, x_rise_l, x_fall_h, x_fall_l = find_independent_indices(x_fixed, 
@@ -21,17 +28,17 @@ def hysteresis_class(x:pd.Series,y:pd.Series,x_fixed:pd.Series):
     y_fixed_rise, y_fixed_fall = y_for_x_fixed(x_rise_h, x_rise_l, 
                                                x_fall_h, x_fall_l, y_norm, 
                                                x_norm, x_fixed)
+    
     rise_area,fall_area,diff_area,h = area_hysteresis_index(x_fixed, 
                                                             y_fixed_rise, 
                                                             y_fixed_fall)
-    # Save the the min and max before forcing the linearity
+
     min_dA=min(diff_area)
     max_dA=max(diff_area)
-    rise_area,fall_area,diff_area,h = force_linearity(rise_area,
-                                                      fall_area, diff_area,h, 
-                                                      x_fixed)
-    
-    return diff_area, h , find_hysteresis_class(x, y, min_dA, max_dA, h)
+    h = h if np.isfinite(h) else 0
+    hyst_class =  find_hysteresis_class(x, y, min_dA, max_dA, h)
+        
+    return diff_area, h , hyst_class
 
 
 def normalize(series:pd.Series):
@@ -42,24 +49,20 @@ def find_independent_indices(x_fixed: pd.Series, x_norm:pd.Series):
     """
     The function indice_x_norm is applied to find indices for x for 
     observations for all values of x_fixed. 
-    """
-    x_rise_h = pd.Series(0, index=range(len(x_fixed)))
-    x_rise_l = pd.Series(0, index=range(len(x_fixed)))
-    x_fall_h = pd.Series(0, index=range(len(x_fixed)))
-    x_fall_l = pd.Series(0, index=range(len(x_fixed)))
-    for k in range(len(x_fixed)):
-        x_norm_list = list(x_norm)
-        indices = [i for i, x in enumerate(x_norm_list) if x == x_fixed[k]]
-        # check if indices is empty
-        empty = not indices
-        if empty:
-            x_rise_h[k], x_rise_l[k], x_fall_h[k], x_fall_l[k] = (
-                    indice_x_norm(x_fixed[k],x_norm))
+    """   
+    x_norm_list = list(x_norm)
+    def get_indices(indices:list, x_selected:float, x_norm_list:list):
+        if len(indices) == 0:
+            return indice_x_norm(x_selected,x_norm)
         else:
-            x_rise_h[k] = x_rise_l[k] = indices[0]
-            x_fall_h[k] = x_fall_l[k] = indices[-1]           
-            
-    return x_rise_h, x_rise_l, x_fall_h, x_fall_l
+            return indices[0], indices[0], indices[-1], indices[-1]
+        
+    x_rise_h, x_rise_l, x_fall_h, x_fall_l = zip(*(
+            get_indices(
+                    np.where(x_norm_list == x_fixed[k])[0], x_fixed[k], x_norm_list) 
+            for k in range(len(x_fixed))))
+           
+    return pd.Series(x_rise_h), pd.Series(x_rise_l), pd.Series(x_fall_h), pd.Series(x_fall_l)
 
 
 def indice_x_norm(x_selected:float, x_norm:pd.Series):
@@ -76,9 +79,7 @@ def indice_x_norm(x_selected:float, x_norm:pd.Series):
     pos_max_x_norm = x_norm.index.get_loc(x_norm.idxmax())
     
     # Calculate the rising limb 
-    delta_rise = pd.Series(np.nan, index=x_norm.index)
-    for i in range(pos_max_x_norm + 1): # plus one as matlab counts different
-        delta_rise.iloc[i] = x_norm.iloc[i] - x_selected
+    delta_rise = x_norm.iloc[:pos_max_x_norm+1] - x_selected
     # Lower than "0" = x_rise_minor
     if (delta_rise.dropna() > 0).all():
         x_rise_major = x_rise_minor = x_fall_major = x_fall_minor = np.nan
@@ -106,9 +107,8 @@ def indice_x_norm(x_selected:float, x_norm:pd.Series):
         x_rise_major = index_position
         
     # Falling limb of the hydrograph
-    delta_fall = pd.Series(np.nan, index=x_norm.index)
-    for j in range(pos_max_x_norm, len(x_norm)):
-        delta_fall[j] = x_norm[j] - x_selected
+    delta_fall = x_norm - x_selected
+    delta_fall.iloc[:pos_max_x_norm] = np.nan
         
     # Hihger than "0" = x_fall_major
     # Find the positive value closest to 0
@@ -128,7 +128,12 @@ def indice_x_norm(x_selected:float, x_norm:pd.Series):
         x_rise_major = x_rise_minor = x_fall_major = x_fall_minor = np.nan
         return x_rise_major, x_rise_minor, x_fall_major, x_fall_minor
     
-    if delta_fall.iloc[x_fall_major+1] < delta_fall.iloc[x_fall_major]:
+   # check if x_fall_major is at the last position
+    if x_fall_major + 1 < len(delta_fall):
+        check = delta_fall.iloc[x_fall_major+1] < delta_fall.iloc[x_fall_major] 
+    else:
+        check = False
+    if check: 
         x_fall_minor = x_fall_major + 1
     else:
         # Find the negative value closest to 0
@@ -182,7 +187,7 @@ def y_for_x_fixed(x_rise_h:pd.Series, x_rise_l:pd.Series, x_fall_h:pd.Series,
     y_fixed_rise = pd.Series(0, index=x_fixed.index)
     y_fixed_fall = pd.Series(0, index=x_fixed.index)
     
-    # Calculate the y values
+    # Calculate the y values    
     for k in range(len(x_fixed)):
         if np.isnan(m_rise[k]):
             y_fixed_rise.iloc[k] = y_norm[x_rise_h[k]]
@@ -217,18 +222,6 @@ def area_hysteresis_index(x_fixed:pd.Series, y_fixed_rise:pd.Series,
     h = diff_area.sum()
 
     return areas_rising, areas_falling, diff_area,h
-
-
-def force_linearity(rise_area:pd.Series, fall_area:pd.Series, 
-                    diff_area:pd.Series, h: float, x_fixed:pd.Series):
-    """not sure what this function is supposed to be doing"""
-    # Forcing linearity (no loop) = 0
-    rise_area.fillna(0, inplace=True)
-    fall_area.fillna(0, inplace=True)
-    diff_area.fillna(0, inplace=True)
-    if np.isnan(h):
-        h=0
-    return rise_area, fall_area, diff_area, h
 
 
 def find_hysteresis_class(x:pd.Series, y:pd.Series, min_dA:float, 
@@ -308,7 +301,6 @@ if __name__ == "__main__":
                              0.55, 0.60, 0.65, 0.70, 0.75, 0.80, 0.85, 0.90, 
                              0.95, 1.00])
         diff_area, h, hyst_class = hysteresis_class(x, y, x_fixed)
-        diff_area = abs(diff_area)
         if hyst_class == right_hyst_class[mapping[sheet]]:
             print("Delivers same class for test data as matlab code")
         else:
@@ -327,11 +319,13 @@ if __name__ == "__main__":
         y2 = [0, 0, 0]
         
         ax2.plot(x_fixed[:-1], diff_area, color="red")
+        ax2.plot(x2, y2, color="black")
         ax2.set_xlabel('Streamflow (-)')
         ax2.set_ylabel('\DeltaA (-)')
         ax2.set_title('Difference between the integrals')
     
     fig.tight_layout()
+
     
     
 
