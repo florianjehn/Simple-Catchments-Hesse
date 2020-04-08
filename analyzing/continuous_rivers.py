f2d0ff672eba09dfaad49feefa8fed4db6e12f09
Florian Ulrich Jehn <florian.u.jehn@envr.jlug.de>
2020-04-08 13:02:00 +0200

    Removed unused code

--- analyzing/continuous_rivers.py
+++ analyzing/continuous_rivers.py
@@ -11,100 +11,9 @@ file_dir = os.path.dirname(os.path.abspath(__file__))
 sys.path.append(os.sep.join(file_dir.split(os.sep)[:-1]))
 
 
-def scatter_violin_lse_all_together(catchments, years, least_squares):           
-    """
-    Plots the attributes of the years and catchments in one big figure.
-        """
-    fig = plt.figure(figsize=(20,30))
-    outer = gridspec.GridSpec(nrows=2, ncols=1, height_ratios=[16,9], hspace=0.15)
-    
-    catchments_grid = gridspec.GridSpecFromSubplotSpec(4,4, subplot_spec=outer[0], wspace=0.2, hspace=0.6)
-    
-    years_grid = gridspec.GridSpecFromSubplotSpec(2,4, subplot_spec=outer[1], wspace=0.2, hspace=0.2)
-    
-    axes = []
-    years.drop(["mean_air_temperature", "soil_temp_C_1991_2018"], inplace=True, axis=1)
-    # Revert catchment columns
-    for axis, catch_year in enumerate([catchments, years]):
-        print(axis)
-        for i,attribute in enumerate(catch_year.columns):
-            print(attribute)
-            if attribute == "gauge":
-                continue
-            iterate = least_squares.index if axis == 1 else least_squares.columns
-            collect = []
-            for single_catch_year in iterate:
-                # Skip catchments with much missing or stepwise data
-                if single_catch_year in ["23950104", "24781159", "24781206", "428832990", "42870057"]:
-                    print("Skipped: " + str(single_catch_year))
-                    continue
-                if axis == 1:
-                    least_square_single = least_squares.loc[single_catch_year,:] 
-                elif axis == 0:
-                    least_square_single = least_squares.loc[:,single_catch_year]
-                least_square_single.name = "catchment_least_square"
-                current_catch_att = catch_year.loc[ int(single_catch_year), attribute]
-                # Make attribute as long as the least squares
-                current_catch_att = pd.Series([current_catch_att] * len(least_square_single), index=least_square_single.index)
-                current_catch_att.name=attribute
-                combined = pd.concat([least_square_single, pd.Series(current_catch_att)],axis=1)
-                collect.append(combined)
-            all_data_attribute = pd.concat(collect,ignore_index=True)
-            ax = plt.Subplot(fig, catchments_grid[i]) if axis == 0 else plt.Subplot(fig, years_grid[i])
-
-            fig = plt.gcf()
-            bonferoni_p_val_correction = 24
-            if all_data_attribute[attribute].dtype != float:
-                sns.violinplot(y="catchment_least_square", data=all_data_attribute, x=attribute,ax=ax, color="lightsteelblue", zorder=5)
-             #   print(all_data_attribute)
-                values_per_group = [col.dropna() for col_name, col in all_data_attribute.groupby(attribute)["catchment_least_square"]]
-    #            for key, val in values_per_group.items():
-    #                print(key)
-    #                print(val)
-                statistic, pval = scipy.stats.f_oneway(*values_per_group)
-                for tick in ax.get_xticklabels():
-                    tick.set_rotation(50)
-                pval = pval*bonferoni_p_val_correction
-                pval = 1 if pval > 1 else pval
-                ax.set_title("P-Value: " + str(round(pval,3)),alpha=0.7)
-                ax.set_xlabel(attribute, alpha=0.7)
-                if attribute == "Land Use [/]":
-                    ax.set_xlim(-0.5,2.5)
-                    
-    
-            else:
-                x = all_data_attribute[attribute].astype(float)
-                y = all_data_attribute["catchment_least_square"]
-                xy = pd.concat([x,y],axis=1)
-                xy.dropna(inplace=True)
-                results = scipy.stats.linregress(xy)           
-                ax = sns.regplot(x,y, marker="o",
-                            scatter_kws={"s":0.2, "facecolor":"steelblue", "edgecolor":"steelblue", "zorder":4, "alpha":0.5},
-                            line_kws={"color":"black", "linewidth":"0.75", "zorder":5}, ax=ax)
-                ax.set_xlabel(attribute,   alpha=0.7)
-                ax.grid(True, color="lightgrey",zorder=0)
-                ax.set_xlim(all_data_attribute[attribute].min(), all_data_attribute[attribute].max())
-                pval = results[3]*bonferoni_p_val_correction
-                pval = 1 if pval > 1 else pval
-                ax.set_title("P-Value: " + str(round(pval,3)),alpha=0.7)
-
-            ax.set_ylabel("Mean Least Square Error [/]",alpha=.7) 
-            # Make nicer
-            plt.setp(ax.get_yticklabels(), alpha=0.7)
-            plt.setp(ax.get_xticklabels(), alpha=0.7)
-            ax.tick_params(axis=u'both', which=u'both',length=0)
-            for spine in ax.spines.values():
-                spine.set_visible(False)
-            
-            
-            axes.append(ax)
-            fig.add_subplot(ax)
-
-    plt.savefig("all_regressions_attributes.png", dpi=200, bbox_inches="tight")
-    plt.close()
             
     
-def scatter_violin_lse_all_only_catchments(catchments, least_squares):           
+def scatter_swarm_lse_all_only_catchments(catchments, least_squares):           
     """
     Plots the scatter and violin plots only for the catchments
     
@@ -219,84 +128,6 @@ def scatter_violin_lse_all_only_catchments(catchments, least_squares):
     plt.savefig("catchment_regressions_attributes.png", dpi=300, bbox_inches="tight")
     plt.close()
             
-
-
-def scatter_violin_least_squares_attribute(catch_year, least_squares, axis):
-    """
-    Plots the attributes of the years or catchments in a scatterplot 
-    with the least squares for those catchments. Axis = 0 --> columns/catchments
-    axis = 1 --> rows/years
-        """
-    for attribute in catch_year.columns:
-        print(attribute)
-        if attribute == "gauge":
-            continue
-        if "temp" in attribute:
-            continue
-        iterate = least_squares.index if axis == 1 else least_squares.columns
-        collect = []
-        for single_catch_year in iterate:
-            # Skip catchments with much missing or stepwise data
-            if single_catch_year in ["23950104", "24781159", "24781206", "428832990", "42870057"]:
-                print("Skipped: " + str(single_catch_year))
-                continue
-            if axis == 1:
-                least_square_single = least_squares.loc[single_catch_year,:] 
-            elif axis == 0:
-                least_square_single = least_squares.loc[:,single_catch_year]
-            least_square_single.name = "catchment_least_square"
-            current_catch_att = catch_year.loc[ int(single_catch_year), attribute]
-            # Make attribute as long as the least squares
-            current_catch_att = pd.Series([current_catch_att] * len(least_square_single), index=least_square_single.index)
-            current_catch_att.name=attribute
-            combined = pd.concat([least_square_single, pd.Series(current_catch_att)],axis=1)
-            collect.append(combined)
-        all_data_attribute = pd.concat(collect,ignore_index=True)
-        
-        ax = plt.gca()
-        fig = plt.gcf()
-        looking_at = "Year Attribute" if axis == 1 else "Catchment Attribute"
-        bonferoni_p_val_correction = 24
-
-        if all_data_attribute[attribute].dtype != float:
-            sns.violinplot(y="catchment_least_square", data=all_data_attribute, x=attribute,ax=ax, color="lightsteelblue", zorder=5)
-         #   print(all_data_attribute)
-            values_per_group = [col.dropna() for col_name, col in all_data_attribute.groupby(attribute)["catchment_least_square"]]
-            statistic, pval = scipy.stats.f_oneway(*values_per_group)
-            for tick in ax.get_xticklabels():
-                tick.set_rotation(50)
-            pval = pval*bonferoni_p_val_correction
-            pval = 1 if pval > 1 else pval
-            ax.set_title(looking_at + ": " + attribute + " pval: " + str(round(pval,2)),alpha=0.7)
-
-        else:
-            x = all_data_attribute[attribute].astype(float)
-            y = all_data_attribute["catchment_least_square"]
-            xy = pd.concat([x,y],axis=1)
-            xy.dropna(inplace=True)
-            results = scipy.stats.linregress(xy)           
-            ax = sns.regplot(x,y, marker="o",
-                        scatter_kws={"s":0.2, "facecolor":"lightsteelblue", "edgecolor":None},
-                        line_kws={"color":"black", "linewidth":"0.75"})
-            pval = results[3]*bonferoni_p_val_correction
-            pval = 1 if pval > 1 else pval
-
-            ax.set_title(looking_at + ": " + attribute + " pval: " +str(round(pval,2)),
-                         alpha=0.7)
-            ax.grid(True, color="lightgrey",zorder=0)
-
-        ax.set_ylabel("Mean Least Square Error [/]",alpha=.7) 
-        # Make nicer
-        plt.setp(ax.get_yticklabels(), alpha=0.7)
-        plt.setp(ax.get_xticklabels(), alpha=0.7)
-        ax.tick_params(axis=u'both', which=u'both',length=0)
-        for spine in ax.spines.values():
-            spine.set_visible(False)
-        
-        ax.set_xlabel(attribute, alpha=0.7)
-        fig.tight_layout()
-        plt.savefig(looking_at + attribute[:-4] + ".png", dpi=200)
-        plt.close()
         
         
 def find_unique_pairs(cats):
@@ -395,31 +226,6 @@ def heatmap_ls(least_squares):
     plt.close()
     
     
-def regplot_kge_lse(lse, kge):
-    """
-    Creates a regplot for all values of the least squares and the KGE for 
-    all catchments and all years. 
-    """
-    new = pd.DataFrame(columns = ["kge", "lse"], index=list(range(2430)))
-    new["kge"] = pd.concat([kge[col] for col in kge]).reset_index(drop=True)
-    new["lse"] = pd.concat([lse[col] for col in lse]).reset_index(drop=True)
-    ax = sns.regplot(y="kge", x="lse", data=new,marker="o",
-                        scatter_kws={"s":0.5, "facecolor":"steelblue", "edgecolor":"steelblue", "zorder":5, "alpha":0.5},
-                        line_kws={"color":"black", "linewidth":"0.75", "zorder":6})
-    results = scipy.stats.linregress(new.dropna())
-    ax.set_title("Relationship Model Efficieny and Catchment Complexity, pval: " +str(round(results[3]*23,3)), alpha=0.7)
-    ax.set_xlabel("Mean Least Square Error [/]", alpha=0.7)
-    ax.set_ylabel("Kling Gupta Efficienty [/]", alpha=0.7)
-    for spine in ax.spines.values():
-        spine.set_visible(False)
-    ax.tick_params(axis=u'both', which=u'both',length=0)
-
-    ax.grid(True, color="lightgrey", zorder=0)
-    plt.setp(ax.get_yticklabels(), alpha=0.7)
-    plt.setp(ax.get_xticklabels(), alpha=0.7)
-    plt.savefig("kge_lse.png", dpi=200)
-    plt.close()
-
 
 if __name__ == "__main__":
    import preprocessing.cleaned_data.create_cleaned_data_table as ccdt
@@ -439,10 +245,5 @@ if __name__ == "__main__":
    years = ccdt.get_attributes_years()
    least_squares = pd.read_csv("least_square_all_catchments.csv", sep=";", index_col=0)
    del(least_squares["41510205"])
-#   heatmap_ls(least_squares)
-#   scatter_violin_least_squares_attribute(catchments, least_squares, 0)
-#   scatter_violin_least_squares_attribute(years, least_squares, 1)
-#   scatter_violin_lse_all_together(catchments, years, least_squares)
-   scatter_violin_lse_all_only_catchments(catchments, least_squares)
-#   kge = pd.read_csv("kge_all_years_catch.csv", sep=";", index_col=0)
-#   regplot_kge_lse(least_squares, kge)
+   heatmap_ls(least_squares)
+   scatter_swarm_lse_all_only_catchments(catchments, least_squares)
--- analyzing/grouped_rivers_years.py
+++ analyzing/grouped_rivers_years.py
@@ -80,7 +80,7 @@ def plot_differences_catchments_years_by_least_squares_only_catchments(catchment
                                      "mid", "variable"]).transpose()
             ax = all_types_by_cat.plot(kind="bar", stacked=True, ax=ax, color=colors, edgecolor="grey", zorder=5)
             handles, labels = ax.get_legend_handles_labels()
-            legend = ax.legend(handles[::-1], labels[::-1], loc=7, fontsize=7.5,bbox_to_anchor=(1.42, 0.5))
+            ax.legend(handles[::-1], labels[::-1], loc=7, fontsize=7.5,bbox_to_anchor=(1.42, 0.5))
             for tick in ax.get_xticklabels():
                 tick.set_rotation(360)
             ax.set_ylabel("Relative Frequency [%]", alpha=0.7)
@@ -148,139 +148,6 @@ def plot_differences_catchments_years_by_least_squares_only_catchments(catchment
     plt.savefig("all_together_most_extreme_catch.png", dpi=300,  bbox_inches="tight")
     plt.close()      
 
-def plot_differences_catchments_years_by_least_squares_all_together(catchments, years, least_squares, amount_homogen):           
-    """
-    Plots the attributes of the years or catchments in a scatterplot 
-    with the least squares for those catchments. Axis = 0 --> columns/catchments
-    axis = 1 --> rows/years
-        """
-    fig = plt.figure(figsize=(20,30))
-    outer = gridspec.GridSpec(nrows=2, ncols=1, height_ratios=[5,2], hspace=0.15)
-    
-    catchments_grid = gridspec.GridSpecFromSubplotSpec(4,3, subplot_spec=outer[0], wspace=0.2, hspace=0.15)
-    
-    years_grid = gridspec.GridSpecFromSubplotSpec(2,3, subplot_spec=outer[1], wspace=0.2, hspace=0.15)
-    axes = []
-
-    for axis, catch_year in enumerate([catchments, years]):
-        print(axis)
-
-        # Get the predominant type for every year/catchment
-        if axis == 0:
-            mean_least_squares = least_squares.mean(axis=0)
-        else:
-            mean_least_squares = least_squares.mean(axis=1)
-            
-        # Find the year/catchment that have the highest and lowest least square error
-        simple_catch_year = mean_least_squares[mean_least_squares < mean_least_squares.quantile(amount_homogen)].index.astype(float)
-        complex_catch_year = mean_least_squares[mean_least_squares > mean_least_squares.quantile(1-amount_homogen)].index.astype(float)
-        most_homogen = {"simple": simple_catch_year, "complex":complex_catch_year}
-            
-        # Create a figure for every attribute
-        for i, att in enumerate(catch_year.columns):
-            ax = plt.Subplot(fig, catchments_grid[i]) if axis == 0 else plt.Subplot(fig, years_grid[i])
-
-            print(att)
-            attributes_for_types = {}
-            for type_catch in most_homogen.keys():
-                homogen_type = most_homogen[type_catch]
-                attributes_for_one_type = catch_year.loc[homogen_type, att]
-                attributes_for_one_type.name = type_catch
-                attributes_for_one_type.reset_index(inplace=True, drop=True)
-                attributes_for_types[type_catch] = attributes_for_one_type
-         #       print(attributes_for_one_type)
-                
-            all_types = pd.concat([attributes_for_types[type_catch] for type_catch in attributes_for_types.keys()],axis=1)
-         #   print(all_types)
-    
-            # if they are not flaot or int they should be categorical
-            if catch_year[att].dtypes != np.float64 and catch_year[att].dtypes != np.int64:
-                all_types_by_cat = pd.concat([all_types.groupby(col).count() for col in all_types.columns],axis=1, sort=True)
-                all_types_by_cat = all_types_by_cat[all_types_by_cat.columns[::-1]]
-                cmap = matplotlib.cm.get_cmap("PuBu")
-                colors = [cmap(i / len(all_types_by_cat.transpose().columns)) for i in range(len(all_types_by_cat.transpose().columns))]
-                ax = all_types_by_cat.transpose().plot(kind="bar", stacked=True, ax=ax, color=colors, edgecolor="grey", zorder=5)
-                legend = ax.legend(loc=10, fontsize=7)
-                for tick in ax.get_xticklabels():
-                    tick.set_rotation(360)
-                ax.set_ylabel("Frequency", alpha=0.7)
-                ax.set_xlabel(att, alpha=0.7)
-
-                
-    
-            else:
-            
-                ax = sns.swarmplot(data=all_types, ax=ax, zorder=5, color="steelblue")
-                ax = sns.boxplot(data=all_types, showcaps=False,boxprops={'facecolor':'None', "edgecolor":"lightgrey"},
-                                 showfliers=False,whiskerprops={'linewidth':0,}, ax=ax, zorder=4, width=0.3)
-                ax.set_ylabel(att, alpha=0.7)
-
-            ax.yaxis.grid(True, color="lightgrey",zorder=0)
-            axes.append(ax)
-            fig.add_subplot(ax)
-            plt.setp(ax.get_yticklabels(), alpha=0.7)
-            plt.setp(ax.get_xticklabels(), alpha=0.7)
-            ax.tick_params(axis=u'both', which=u'both',length=0)
-            for spine in ax.spines.values():
-                spine.set_visible(False)
-            
-    fig = plt.gcf()
-    plt.savefig("all_together_most_extreme.png", dpi=150,  bbox_inches="tight")
-    plt.close()      
-
-
-def plot_differences_catchments_years_by_least_squares(least_squares, attributes, amount_homogen, year_catch):
-    """
-    Plots the difference in the catchments (axis=0) or year (axis=1) in 
-    regard to the catchmetns with the highest/lowest median least square error
-    """
-
-    # Get the predominant type for every year/catchment
-    if year_catch == "catch":
-        mean_least_squares = least_squares.mean(axis=0)
-    elif year_catch == "year":
-        mean_least_squares = least_squares.mean(axis=1)
-        
-    # Find the year/catchment that have the highest and lowest least square error
-    simple_catch_year = mean_least_squares[mean_least_squares < mean_least_squares.quantile(amount_homogen)].index.astype(float)
-    complex_catch_year = mean_least_squares[mean_least_squares > mean_least_squares.quantile(1-amount_homogen)].index.astype(float)
-    most_homogen = {"simple": simple_catch_year, "complex":complex_catch_year}
-        
-    # Create a figure for every attribute
-    for att in attributes.columns:
-        attributes_for_types = {}
-        for type_catch in most_homogen.keys():
-            homogen_type = most_homogen[type_catch]
-            attributes_for_one_type = attributes.loc[homogen_type, att]
-            attributes_for_one_type.name = type_catch
-            attributes_for_one_type.reset_index(inplace=True, drop=True)
-            attributes_for_types[type_catch] = attributes_for_one_type
-     #       print(attributes_for_one_type)
-            
-        all_types = pd.concat([attributes_for_types[type_catch] for type_catch in attributes_for_types.keys()],axis=1)
-
-        # if they are not flaot or int they should be categorical
-        if attributes[att].dtypes != np.float64 and attributes[att].dtypes != np.int64:
-            all_types_by_cat = pd.concat([all_types.groupby(col).count() for col in all_types.columns],axis=1)
-            all_types_by_cat = all_types_by_cat[all_types_by_cat.columns[::-1]]
-            ax = all_types_by_cat.transpose().plot(kind="bar", stacked=True)
-            ax.set_title(year_catch + "; attribute: " + att+ "; n="+str(len(simple_catch_year)))
-
-        else:
-            ax = sns.swarmplot(data=all_types)
-            ax = sns.boxplot(data=all_types, showcaps=False,boxprops={'facecolor':'None'},
-        showfliers=False,whiskerprops={'linewidth':0})
-      #      ax = all_types.plot(kind="box") 
-       #     print(all_types)
-            statistic, p_value = scipy.stats.ranksums(all_types["simple"], all_types["complex"])
-            ax.set_title(year_catch + "; attribute: " + att + "; p_val: "+str(np.round(p_value,decimals=3)) + "; n="+str(len(simple_catch_year)))
-            
-            
-        fig = plt.gcf()
-        fig.tight_layout()
-        plt.savefig(year_catch + "_attribute_" + att[:-4]+ ".png", dpi=150,  bbox_inches="tight")
-        plt.close()      
-        
         
 if __name__ == "__main__":
    import preprocessing.cleaned_data.create_cleaned_data_table as ccdt
@@ -297,16 +164,10 @@ if __name__ == "__main__":
                         'Aquifer Conductivity [/]', 'Geology Type [/]', 'Ground Water Recharge [mm]',        'Permeability [/]'
 
         ], axis=1)
-   years = ccdt.get_attributes_years()
    least_squares = pd.read_csv("least_square_all_catchments.csv", sep=";", index_col=0)
-#   plot_differences_catchments_years_by_least_squares(least_squares, catchments , 0.2, "catch")
-#   plot_differences_catchments_years_by_least_squares(least_squares, years, 0.2, "year")
    
    # Exclude non significant attributes
-   years.drop(['soil_temp_C_1991_2018', 'mean_air_temperature',
-               'Precipitation [mm]', 'Snow Fraction [%]',
-               'Aridity [/]'],inplace=True, axis=1)
+
    catchments.drop(['Land Use [/]', 'Area [km²]', 'Soil Depth [m]',
                     'Slope [/]'],inplace=True, axis=1)
- #  plot_differences_catchments_years_by_least_squares_all_together(catchments, years, least_squares, 0.2)
    plot_differences_catchments_years_by_least_squares_only_catchments(catchments, least_squares, 0.2)
